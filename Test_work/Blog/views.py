88d6bcd9308794d58ae49ce2b9bf3b7ddfe7e732
Konstantin Shchegolev <konstantinshchegolev@Konstantins-MacBook-Air.local>
2021-04-14 21:14:57 +0300

    app: Blog; base, index - templates

---
+++ Test_work/Blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+def index(request):
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    return render(
+        request,
+        'index.html',
+        context={
+            'num_visits': num_visits,
+        }
+    )
+
+
+
